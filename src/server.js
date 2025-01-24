600a327162aeba7ac25016a8de4540356196c3c1
Lalit Tembhare <138137379+Lalla2000@users.noreply.github.com>
2025-01-24 15:59:36 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -13,5 +13,5 @@ app.use(bodyParser.json())
 endpoints(app)
 
 app.listen(3001, () => {
-  console.log('Server started at http://13.229.65.4:3000')
+  console.log('Server started at http://13.201.8.195:3000')
 })
